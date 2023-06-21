17d3a05be7078aceffe96b0619d3d7c3dd61c45b
Syed Wafi Hasan <syedwafihasan@gmail.com>
2023-06-21 21:07:25 +0530

    Completed BackEnd part

--- src/main/java/in/co/swh/movies/MovieController.java
+++ src/main/java/in/co/swh/movies/MovieController.java
@@ -15,15 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+	
 	@Autowired
 	private MovieService movieService;
+	
 	@GetMapping
 	public ResponseEntity<List<Movie>> getAllMovies() {
 		return new ResponseEntity<List<Movie>>(movieService.allMovies(), null, HttpStatus.OK);
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
-		return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), null, HttpStatus.OK);
+	@GetMapping("/{imdbId}")
+	public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+		return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), null, HttpStatus.OK);
 	}
 }
--- src/main/java/in/co/swh/movies/MovieService.java
+++ src/main/java/in/co/swh/movies/MovieService.java
@@ -3,19 +3,20 @@ package in.co.swh.movies;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
+	
 	@Autowired
 	private MovieRepository movieRepository;
+	
 	public List<Movie> allMovies() {
 		return movieRepository.findAll();
 	}
 
-	public Optional<Movie> singleMovie(ObjectId id) {
-		return movieRepository.findById(id);
+	public Optional<Movie> singleMovie(String imdbId) {
+		return movieRepository.findMovieByImdbId(imdbId);
 	}
 }
